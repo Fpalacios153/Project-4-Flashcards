566580e644e8faf8525025b9bdd66e4f149b7ac6
Francisco Palacios <Fpalacios926@gmail.com>
2024-08-12 23:05:40 -0700

    inital commit with serverside code done

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,10 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -10,8 +17,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+
+      <html lang="en">
+        <body className={inter.className}>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE,
+    authDomain: process.env.NEXT_PUBLIC_API_KEY_authDomain,
+    projectId: process.env.NEXT_PUBLIC_API_KEY_projectId,
+    storageBucket: process.env.NEXT_PUBLIC_API_KEY_storageBucket,
+    messagingSenderId: process.env.NEXT_PUBLIC_API_KEY_messagingSenderId,
+    appId: process.env.NEXT_PUBLIC_API_KEY_appId,
+    measurementId: process.env.NEXT_PUBLIC_API_KEY_measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const db = getFirestore(app);
+export default db;
