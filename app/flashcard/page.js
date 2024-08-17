2ec295c1e5bff9b2712d2f754676818af548ec99
Wendy Chew <wendyyychew@gmail.com>
2024-08-16 18:43:15 -0700

    added animation to flip the card

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -61,7 +61,34 @@ export default function FlashcardSet() {
                         <Card>
                             <CardActionArea onClick={() => handleCardClick(flashcard.id)}>
                                 <CardContent>
-                                    <Box sx={{ /* Styling for flip animation */ }}>
+                                    <Box sx={{ 
+                                        perspective: '1000px',
+                                        '& > div':{
+                                            transition: 'transform 0.6s',
+                                            transformStyle: 'preserve-3d', 
+                                            position: 'relative',
+                                            width: '100%' ,
+                                            height: '200px',
+                                            boxShadow: '0 4px 8px 0 rgba(0,0,0, 0.2)',
+                                            transform: flipped [flashcard.id]
+                                            ? 'rotateY(180deg)'
+                                            : 'rotatey(0deg)',
+                                        },
+                                        '& > div > div':{
+                                            postion: 'absolute',
+                                            width: '100%' ,
+                                            height: '100%',
+                                            backfaceVisibility: 'hidden',
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            padding: 2,
+                                            boxSizing: 'border-box',
+                                        },
+                                        '& > div > div:nth-of-type(2)':{
+                                            transform: 'rotateY(180deg)',
+                                        },
+                                    }}>
                                         <div>
                                             <div>
                                                 <Typography variant="h5" component="div">
